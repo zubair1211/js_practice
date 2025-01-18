6f7d1ac238f9bb160056ac2516c638ab91f8e236
ZUBAIR <zubairtahir591@gmail.com>
2025-01-18 14:22:57 +0000

    02

---
+++ 01-basics/02-basics.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//alert("hello")//
+let name = "zubair"
+let age = 24
+let isLoggedIn = false
+let state = null
+console.log(typeof age)
---
+++ 01-basics/variables.js
@@ -0,0 +1,7 @@
+const accountId =14455
+let accountEmail ="zub1@gmail.com"
+var accountPassword = "123"
+accountCity = "lahore"
+console.log(accountId);
+accountEmail = "bbb"
+console.table([accountEmail,accountId,accountPassword])
